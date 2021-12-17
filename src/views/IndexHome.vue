7838ff6803c6d825632e29179f2fdef118b75371
 Pavel <pavel@iMac-Pavel.local>
2021-12-17 13:55:23 +0300

    Добавил модуль - Filter

--- src/views/IndexHome.vue
+++ src/views/IndexHome.vue
@@ -2,12 +2,20 @@
   <div>
     <h1 class="visually-hidden">Главная</h1>
     <div class="onlineshop-app__blueline"></div>
-    <div class="onlineshop-app__wrapper"></div>
+    <div class="onlineshop-app__wrapper">
+      <TheFilter></TheFilter>
+    </div>
   </div>
 </template>
 
 <script>
+import TheFilter from "@/modules/filter/components/TheFilter";
+
 export default {
   name: "IndexHome",
+
+  components: {
+    TheFilter,
+  },
 };
 </script>
