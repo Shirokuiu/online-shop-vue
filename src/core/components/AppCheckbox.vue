44057ec5087ce14f8bbc6d15b343e5d96be859b0
 Pavel <pavel@iMac-Pavel.local>
2021-12-20 20:39:29 +0300

    Создал компонент с категорией - Недвижимость + компонент AppCheckbox для испольщования по всему проекту

---
+++ src/core/components/AppCheckbox.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="checkbox">
+    <input
+      class="visually-hidden"
+      type="checkbox"
+      :name="name"
+      :value="value"
+      :id="id"
+    />
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppCheckbox",
+
+  props: {
+    id: {
+      type: String,
+      default: "checkbox",
+    },
+
+    value: {
+      type: String,
+      default: "checkbox",
+    },
+
+    name: {
+      type: String,
+      default: "checkbox",
+    },
+  },
+};
+</script>
