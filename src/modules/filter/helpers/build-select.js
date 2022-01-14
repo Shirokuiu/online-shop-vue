f8bfdbce641317dfc349eb4c19f1b5292e37a559
 Pavel <pavel@iMac-Pavel.local>
2022-01-14 14:46:54 +0300

    Дописал комментарий к функции build-select.js

--- src/modules/filter/helpers/build-select.js
+++ src/modules/filter/helpers/build-select.js
@@ -1,6 +1,6 @@
 /**
  *
- * @param selectValues - Массив строк значений для селекта ['all', 'estate']
+ * @param selectValues - Массив строк значений для селекта ['all', 'estate', и тп]
  * @param selectKeyMap - Словарь для вывода текста согласно значениям из selectValues
  * @param activeValue - Выставление активного значение в селекте
  * @returns {[{id: number, value: string, isSelected: boolean, text: string}]}
