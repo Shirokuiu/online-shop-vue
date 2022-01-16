5d71e27667852a201a4259685f7184fef077d176
Павел Осипов <pavelosipov@MacBook-Air-Pavel.local>
2022-01-16 21:49:14 +0300

    Сделал группу чекбоксов + отрефакторил добавление квери параметров в адресную строку

--- src/modules/filter/store/camera/camera.store.js
+++ src/modules/filter/store/camera/camera.store.js
@@ -1,13 +1,21 @@
-import { buildSelect, setActiveSelect } from "@/modules/filter/helpers";
 import {
+  buildCheckboxes,
+  buildSelect,
+  setActiveCheckbox,
+  setActiveSelect,
+} from "@/modules/filter/helpers";
+import {
+  checkboxesCameraValues,
+  CheckboxLabel,
   ResolutionMatrixMap,
   resolutionMatrixValues,
   ResolutionVideoMap,
   resolutionVideoValues,
-  TypesCameraMutation,
+  TypesResolutionMutation,
   TypesCameraState,
 } from "@/modules/filter/constants";
 import {
+  SET_ACTIVE_CAMERA_TYPE,
   SET_ACTIVE_MATRIX,
   SET_ACTIVE_VIDEO,
 } from "@/modules/filter/store/camera/mutation-types";
@@ -16,6 +24,11 @@ export default {
   namespaced: true,
 
   state: () => ({
+    cameraTypes: buildCheckboxes({
+      values: checkboxesCameraValues,
+      name: "camera-type",
+      label: CheckboxLabel,
+    }),
     resolutionMatrix: buildSelect({
       selectValues: resolutionMatrixValues,
       selectKeyMap: ResolutionMatrixMap,
@@ -29,6 +42,10 @@ export default {
   }),
 
   mutations: {
+    [SET_ACTIVE_CAMERA_TYPE](state, updatedValues) {
+      state.cameraTypes = updatedValues;
+    },
+
     [SET_ACTIVE_MATRIX](state, updatedMatrix) {
       state.resolutionMatrix = updatedMatrix;
     },
@@ -45,7 +62,16 @@ export default {
         activeOptionValue,
       });
 
-      commit(TypesCameraMutation[type], updatedValues);
+      commit(TypesResolutionMutation[type], updatedValues);
+    },
+
+    setActiveCameraType({ state, commit }, checkedValues) {
+      const updatedValues = setActiveCheckbox({
+        checkboxes: state.cameraTypes,
+        checkedValues,
+      });
+
+      commit(SET_ACTIVE_CAMERA_TYPE, updatedValues);
     },
   },
 };
