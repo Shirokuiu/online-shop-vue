1c8ace856abad5e28d532c20b10dcab63eedbde1
Павел Осипов <pavelosipov@MacBook-Air-Pavel.local>
2022-01-20 10:55:47 +0300

    Привязал к стору и сделал изменения в адресной строке для фильтра -  Минимальная площадь

--- src/modules/filter/components/Camera.vue
+++ src/modules/filter/components/Camera.vue
@@ -59,7 +59,7 @@ import AppSelect from "@/core/components/AppSelect";
 import WithAddQueryParam from "@/modules/filter/hocs/WithAddQueryParam";
 import { mapActions, mapState } from "vuex";
 import { TypesResolution } from "@/modules/filter/constants";
-import { parseQueryParamsAndMutate } from "@/modules/filter/helpers";
+import { buildQueryParamsByArray } from "@/modules/filter/helpers";
 
 export default {
   name: "Camera",
@@ -91,11 +91,8 @@ export default {
     ]),
 
     onCameraTypeChange(checkedValues, cb = () => undefined) {
-      parseQueryParamsAndMutate({
-        checkedValues,
-        forHocChangeCb: cb,
-        storeAction: () => this.setActiveCameraType(checkedValues),
-      });
+      cb(buildQueryParamsByArray(checkedValues));
+      this.setActiveCameraType(checkedValues);
     },
   },
 };
